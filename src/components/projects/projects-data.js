002b479635693c5e2aad30110d2961ada70f7c5c
bhasidhshaik <basidhshaik85@gmail.com>
2025-08-25 22:03:17 +0530

    Update projects

--- src/components/projects/projects-data.js
+++ src/components/projects/projects-data.js
@@ -2,7 +2,15 @@ import RecipeFinder from "../../assets/recipe-finder.png"
 import Portfolio from "../../assets/portfolio.png"
 import SocialMedia from "../../assets/social-media.png"
 import CollegeSite from "../../assets/college-site.png"
+import SangeeTru from "../../assets/sangeetru.png"
 const projects = [
+   {
+      title: "SangeeTru - Share Lyrics with Emotions",
+      technologies: ["Next JS", "Tailwind CSS", "Vercel", ],
+      githubLink: "https://github.com/bhasidhshaik/sangeetru",
+      liveLink: "https://sangeetru.vercel.app/",
+      imgUrl: SangeeTru,
+    },
   {
     title: "Social Media",
     technologies: ["ReactJS", "ExpressJS", "MongoDB", "NodeJS"],
@@ -19,17 +27,11 @@ const projects = [
       },
     {
       title: "RUCE College Website",
-      technologies: ["JavaScript", "PHP", "HTML & CSS"],
+      technologies: ["JavaScript", "PHP", "HTML & CSS", "Bootstrap"],
       githubLink: "/", 
       liveLink: "http://ruce.ac.in/",
       imgUrl: CollegeSite,
     },
-    {
-      title: "Personal Website",
-      technologies: ["ReactJS", "CSS"],
-      githubLink: "https://github.com/bhasidhshaik/personal_website",
-      liveLink: "/",
-      imgUrl: Portfolio,
-    },
+   
   ];
 export default projects  
